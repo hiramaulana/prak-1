fe1bc923ced0a0c0db6121e6a81c0f279db32766
HRM <hiramaulana@gmail.com>
2021-08-14 18:48:24 +0700

    create class database connection

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,13 @@
 <?php 
 
+    class Database{
 
+        var $host = "localhost";
+        var $username = "root";
+        var $password = "";
+        var $db = "PL2_prak-1";
+        var $connection;
+
+    }
 
 ?>

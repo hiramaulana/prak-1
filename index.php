299cfffde7cb68e065f8e67ca0b4aeeed511e32a
HRM <hiramaulana@gmail.com>
2021-08-14 23:16:17 +0700

    create index for menu

---
+++ index.php
@@ -0,0 +1,29 @@
+<h1> MENU </h1> 
+
+<table>
+    <tr>
+        <td>No</td>
+        <td>Keterangan</td>
+        <td>Link</td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Daftar Mahasiswa</td>
+        <td><a href="latihan1.php">link</a></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Input Mahasiswa</td>
+        <td><a href="latihan2.php">link</a></td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Update Mahasiswa</td>
+        <td><a href="latihan3.php">link</a></td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Delete Mahasiswa</td>
+        <td><a href="latihan4.php">link</a></td>
+    </tr>
+</table>

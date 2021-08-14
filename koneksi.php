0fd00a65574e75f66381bf79817230dc12a5103e
HRM <hiramaulana@gmail.com>
2021-08-14 18:52:01 +0700

    function connect koneksi

--- koneksi.php
+++ koneksi.php
@@ -6,7 +6,12 @@
         var $username = "root";
         var $password = "";
         var $db = "PL2_prak-1";
-        var $connection;
+        var $connection; 
+
+        function Connect(){
+            $this->connection=mysqli_connect($this->host, $this->username, $this->password, $this->db);
+            return $this->connection;
+        }
 
     }
 
